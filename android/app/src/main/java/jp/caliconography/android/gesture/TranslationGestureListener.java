bae9e110c57ea76567cd1d27d5a7891e90a8e097
AbeHaruhiko <caliconography@gmail.com>
2014-10-10 18:51:32 +0900

    android: 画像のドラッグを可能に。

---
+++ android/app/src/main/java/jp/caliconography/android/gesture/TranslationGestureListener.java
@@ -0,0 +1,12 @@
+package jp.caliconography.android.gesture;
+
+public class TranslationGestureListener {
+    public void onTranslation(TranslationGestureDetector detector) {
+    }
+
+    public void onTranslationBegin(TranslationGestureDetector detector) {
+    }
+
+    public void onTranslationEnd(TranslationGestureDetector detector) {
+    }
+}

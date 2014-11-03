b0f0af05612692f9965ce5019bd164e87424745b
AbeHaruhiko <caliconography@gmail.com>
2014-11-03 13:59:42 +0900

    android: 不要コメント削除

--- android/app/src/main/java/jp/caliconography/one_liners/fragments/PhotoDetailFragment.java
+++ android/app/src/main/java/jp/caliconography/one_liners/fragments/PhotoDetailFragment.java
@@ -476,17 +476,11 @@ public class PhotoDetailFragment extends Fragment {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == android.R.id.home) {
-//            // This ID represents the Home or Up button. In the case of this
-//            // activity, the Up button is shown. Use NavUtils to allow users
-//            // to navigate up one level in the application structure. For
-//            // more details, see the Navigation pattern on Android Design:
-//            //
-//            // http://developer.android.com/design/patterns/navigation.html#up-vs-back
-//            //
-//            NavUtils.navigateUpTo(this.getActivity(), new Intent(this.getActivity(), BookDetailActivity.class));
+
             FragmentManager fm = getActivity().getFragmentManager();
             fm.popBackStack(BookDetailFragment.TAG, FragmentManager.POP_BACK_STACK_INCLUSIVE);
             return true;
+
         } else if (id == R.id.save_photo) {
 
             showProgressBar();

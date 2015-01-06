898d31cf93626886b9045f8fecc8bba3663b2091
AbeHaruhiko <caliconography@gmail.com>
2015-01-06 23:21:00 +0900

    android: Detailのページ番号、レビューの入力・保存のテスト追加。

--- android/app/src/androidTest/java/jp/caliconography/one_liners/activities/BookDetailActivityTest.java
+++ android/app/src/androidTest/java/jp/caliconography/one_liners/activities/BookDetailActivityTest.java
@@ -1,6 +1,5 @@
 package jp.caliconography.one_liners.activities;
 
-import android.content.Intent;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.ActivityInstrumentationTestCase2;
@@ -10,14 +9,22 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import jp.caliconography.one_liners.model.parseobject.Review;
+import jp.caliconography.one_liners.R;
+import jp.caliconography.one_liners.viewaction.InputTextAction;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 @RunWith(AndroidJUnit4.class)
 public class BookDetailActivityTest extends ActivityInstrumentationTestCase2<BookDetailActivity> {
 
+    public static final String STRING_999_TO_BE_TYPED = "999";
+    public static final String STRING_OVER_140_CHARS = "１２３４５６７８９０\n１２３４５６７８９０\n１２３４５６７８９０\n１２３４５６７８９０\n１２３４５６７８９０\n１２３４５６７８９０\n１２３４５６７８９０\n１２３４５６７８９０\n１２３４５６７８９０\n１２３４５６７８９０\n１２３４５６７８９０\n１２３４５６７８９０\n１２３４５６７８９０\n１２３４５６７８９０\n１２３４５６７８９０\n１２３４５６７８９０\n１２３４５６７８９０\n１２３４５６７８９０\n";
     private BookDetailActivity mActivity;
 
     public BookDetailActivityTest() {
@@ -28,9 +35,9 @@ public class BookDetailActivityTest extends ActivityInstrumentationTestCase2<Boo
     public void setUp() throws Exception {
         super.setUp();
         injectInstrumentation(InstrumentationRegistry.getInstrumentation());
-        Intent detailIntent = new Intent(getActivity(), BookDetailActivity.class);
-        detailIntent.putExtra(Review.KEY_OBJECT_ID, "xjTbn1JchZ");
-        setActivityIntent(detailIntent);
+//        Intent detailIntent = new Intent(getActivity(), BookDetailActivity.class);
+//        detailIntent.putExtra(Review.KEY_OBJECT_ID, "xjTbn1JchZ");
+//        setActivityIntent(detailIntent);
         mActivity = getActivity();
     }
 
@@ -47,7 +54,23 @@ public class BookDetailActivityTest extends ActivityInstrumentationTestCase2<Boo
     }
 
     @Test
-    public void testOnPageTextChanged() throws Exception {
+    public void ページに数字を入力するとActivityのReviewオブジェクトに保存される() throws Exception {
+        onView(withId(R.id.txt_page))
+                .perform(typeText(STRING_999_TO_BE_TYPED), closeSoftKeyboard());
+        assertThat(mActivity.getCurrentReview().getPage(), is(STRING_999_TO_BE_TYPED));
+    }
 
+    @Test
+    public void レビューに入力するとActivityのReviewオブジェクトに保存される() throws Exception {
+        onView(withId(R.id.txt_review))
+                .perform(typeText(STRING_999_TO_BE_TYPED), closeSoftKeyboard());
+        assertThat(mActivity.getCurrentReview().getReviewText(), is(STRING_999_TO_BE_TYPED));
+    }
+
+    @Test
+    public void レビューに140文字以上入力すると140文字分ActivityのReviewオブジェクトに保存される() throws Exception {
+        onView(withId(R.id.txt_review))
+                .perform(new InputTextAction(STRING_OVER_140_CHARS), closeSoftKeyboard());
+        assertThat(mActivity.getCurrentReview().getReviewText(), is(STRING_OVER_140_CHARS.substring(0, 140)));
     }
 }
